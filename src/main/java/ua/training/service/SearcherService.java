5f0586c07a3f9b61f3e2157cae49501979db0b5b
Alex Litvin <alexlitvin6969@gmail.com>
2018-07-26 09:01:23 +0300

    delete wrong implementation SearcherService

--- src/main/java/ua/training/service/SearcherService.java
+++ src/main/java/ua/training/service/SearcherService.java
@@ -6,35 +6,11 @@ import ua.training.model.Transparency;
 import ua.training.service.interfaces.Searcher;
 
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class SearcherService implements Searcher {
+
     @Override
-    public List<Gemstone> findGemstonesByTransparency(Necklace necklace, Transparency...transparencies) {
-        int count = transparencies.length;
-        Stream<Gemstone> gemstones = necklace.getNecklace().stream();
-        List<Gemstone> gemstoneList = null;
-        switch (count) {
-            case 1:
-                gemstoneList = gemstones
-                        .filter(gemstone -> gemstone.getTransparency().equals(transparencies[0]))
-                        .collect(Collectors.toList());
-                break;
-            case 2:
-                gemstoneList = gemstones
-                        .filter(gemstone -> gemstone.getTransparency().equals(transparencies[0])
-                                || gemstone.getTransparency().equals(transparencies[1]))
-                        .collect(Collectors.toList());
-                break;
-            case 3:
-                gemstoneList = gemstones
-                        .filter(gemstone -> gemstone.getTransparency().equals(transparencies[0])
-                                || gemstone.getTransparency().equals(transparencies[1])
-                                || gemstone.getTransparency().equals(transparencies[2]))
-                        .collect(Collectors.toList());
-                break;
-        }
-        return gemstoneList;
+    public List<Gemstone> findGemstonesByTransparency(Necklace necklace, Transparency... transparencies) {
+        return null;
     }
 }
