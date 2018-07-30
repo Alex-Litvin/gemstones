04ba82455567d9024c54fa42fdcc29029c770dc2
Alex Litvin <alexlitvin6969@gmail.com>
2018-07-30 19:48:27 +0300

    refactor CostCalculatorService

--- src/main/java/ua/training/service/CostCalculatorService.java
+++ src/main/java/ua/training/service/CostCalculatorService.java
@@ -5,6 +5,7 @@ import ua.training.model.Necklace;
 import ua.training.service.interfaces.CostCalculator;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -14,20 +15,19 @@ public class CostCalculatorService implements CostCalculator {
     @Override
     public BigDecimal calcCostNecklace(Necklace necklace) {
         return necklace.getGemstones().stream()
-                .map(g -> calcCostOneGemstone(g.getCarat(), g.getCaratPrice(), g.getTransparency().getCostFactor()))
+                .map(Gemstone::getTotalCost)
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
     }
 
     @Override
     public BigDecimal calcCostOneGemstone(BigDecimal carat, BigDecimal price, BigDecimal costFactor) {
-        return carat.multiply(price).multiply(costFactor);
+        return carat.multiply(price).multiply(costFactor, new MathContext(2));
     }
 
     @Override
     public Map<Gemstone, BigDecimal> getGemstonePriceMap(Necklace necklace) {
         return necklace.getGemstones().stream()
-                .collect(Collectors.toMap(Function.identity(),
-                        g -> calcCostOneGemstone(g.getCarat(), g.getCaratPrice(), g.getTransparency().getCostFactor())));
+                .collect(Collectors.toMap(Function.identity(), Gemstone::getTotalCost));
 
     }
 }
